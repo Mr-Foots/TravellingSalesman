dd824fa12a4af8c2df6a9d2a36b1d1baf545e08e
Mr-Foots <jackbischoff12@gmail.com>
2015-11-01 12:30:58 -0600

    Finished fitnessEval function. Fixed sorting bug. Added Commenting

--- testSolvers/tspEvo.py
+++ testSolvers/tspEvo.py
@@ -7,70 +7,76 @@ class GeneticOperations():
 		self.params = {
 			'populationSize': 20,
 			'tournamentNumber': 3,
-			'generationNumber': 20
+			'generationNumber': 1
 		}
 	def distance(self, p1, p2):
+		#calculates distance from two points
 		d = (((p2[0] - p1[0])**2) + ((p2[1] - p1[1])**2))**.5
 		return int(d)
 
 
 
 	def tournamentSelection(self, population):
+		#randomly selects n number of candidates from the population
 		selectedIndividuals = random.sample(population, self.params['tournamentNumber'])
-		self.fitnessEvaluation(selectedIndividuals)
+		mates = self.fitnessEvaluation(selectedIndividuals)		#Sends the selected into fitnesseval, returns the mating candidates
 
+		return mates
 
 
 	def fitnessEvaluation(self, selectedIndividuals):
 		fitnessLink = {}
 		fitnessRanked = []
 
-		print(selectedIndividuals,end='\n\n')
+		#loops through the candidates to calculate their distance
 		for index, candidiate in enumerate(selectedIndividuals):
 			pos = 0
 			travelCost = 0
+
+			#loop will go through each node and calculate the distance between the two of them
 			while pos != len(candidiate)-1:
 				p1 = Nodes[str(candidiate[pos])]
 				p2 = Nodes[str(candidiate[pos+1])]
-				travelCost += self.distance(p2, p1)
+				travelCost += self.distance(p2, p1) #distance formula
 				pos += 1
 
-			fitnessLink[str(travelCost)] = candidiate
+			fitnessLink[str(travelCost)] = candidiate #create a dictionary for easy selection and matching to actual sequence
 
-
-		fitnessRanked = sorted(fitnessLink)
-		mate = (fitnessLink[fitnessRanked[0]], fitnessLink[fitnessRanked[1]])
-
-		print(fitnessLink)
-		print(fitnessRanked)
-		print(mate)
-		print('\n')
+		#to find the sequences with the lowest travel time, the fitness values are sorted and matched aganist the orginal dictionary
+		fitnessRanked = sorted(map(int, list(fitnessLink.keys())))
+		mate = (fitnessLink[str(fitnessRanked[0])], fitnessLink[str(fitnessRanked[1])])
+		
+		return mate
 
 
 
-
-
-
-
-	def mutation(self):
-		pass
 
 	def crossover(self):
+		pass
+
+	def mutation(self):
 		pass
 
 
 
 def generatePopulation(size,eleSize):
-	P = []
-	lang = tuple([ x for x in range(2,eleSize+1) ]) #generating nodes
+	
+	P = [] #holds population
+	lang = tuple([ x for x in range(2,eleSize+1) ]) #generating matching node names
+	
+	#Loop creates the n number of candidates
 	for candidiate in range(0, size):
+
 		chromosome = []
 		poss = list(lang)
+
+		#loops to create visiting sequence
 		while len(poss) != 0:
-			selected = random.choice(poss)
-			del poss[poss.index(selected)]
+
+			selected = random.choice(poss) #randomly chooses a possible node
+			del poss[poss.index(selected)] #deletes that node so it doesnt repeat
 			chromosome.append(selected)
-		P.append(chromosome)
+		P.append(chromosome)		
 
 	return P
 
@@ -78,11 +84,15 @@ def generatePopulation(size,eleSize):
 
 def Evolution(population, params):
 
-	for x in range(0, 1):
 
+	for x in range(0, generationNumber):
+		mates = []
 
-		for x in range(0, int(params['populationSize']/2)):
-			selected = G.tournamentSelection(population)
+		#run for half the population. Each mate pair will make two children
+		for x in range(0, int(params['populationSize']/2)): 
+			mates.append(G.tournamentSelection(population))
+		while 
+
 
 
 

e44a50e8c16d16ceb2a15dcc195c656d1d3bde85
Mr-Foots <jackbischoff12@gmail.com>
2015-11-01 12:30:00 -0600

    Deleted instance parser code. May come back later

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-from testTools.tspInstanceParser import parseFile
+
 import testSolvers.tspEvo as evo
 
-
-nodes = []
-
-f = open('testCases/berlin52.txt', 'r')
-p = parseFile(f)
-
-nodes = p[1]['nodes']
 evo.main()
